9abcb243d6e21e05649969e3ad346f72905f9982
Nandan Varma <nandanvarma.me@gmail.com>
2026-07-19 21:13:59 -0700

    fix: update Content Security Policy to allow blob URLs for images

--- vite.config.ts
+++ vite.config.ts
@@ -53,7 +53,7 @@ const config = defineConfig({
 							"default-src 'self'",
 							"script-src 'self' 'unsafe-inline'",
 							"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-							"img-src 'self' data: https:",
+							"img-src 'self' data: blob: https:",
 							"font-src 'self' https://fonts.gstatic.com",
 							"connect-src 'self'",
 							"frame-ancestors 'none'",
